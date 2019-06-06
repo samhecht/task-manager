283545e84b5f155e96d3244a5e864e81faada30e
Sammy Hecht <srh2kq@virginia.edu>
2019-06-06 10:03:02 -0400

    setting up file structure

--- task-manager/app.js
+++ task-manager/app.js
@@ -1,9 +1,40 @@
 const express = require('express');
 const app = express();
 const port = 9000;
+const mysql = require('mysql');
+const puresql = require('puresql');
+const cors = require('cors');
+require('dotenv').config();
+
+
+const dbPassword = process.env.REACT_APP_DB_PASSWORD;
+app.use(cors());
+
+const connection = mysql.createConnection({
+	host : 'localhost',
+	port : 3306,
+	user : 'root',
+	password : dbPassword,
+	database : 'task_manager'
+});
+
+const adapter = puresql.adapters.mysql(connection);
+const queries = puresql.loadQueries('user.sql');
 
 app.get('/', (req, res) => {
-	res.send("hello world");
+	res.send('hello world');
+});
+
+app.get('/allUsers', (req, res) => {
+	async function getAllUsers() {
+		try {
+			const users = await queries.get_all_users({}, adapter);
+			res.send(users);
+		} catch {
+			res.send('couldn\'t get users');
+		}
+	}
+	getAllUsers();
 });
 
 app.listen(port, () => {
---
+++ task-manager/user.sql
@@ -0,0 +1,3 @@
+--name: get_all_users
+SELECT *
+FROM users
