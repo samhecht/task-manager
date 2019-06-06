ba9c96c3b8f71345f17fbf1acd2ce44d7346cd3f
Sammy Hecht <srh2kq@virginia.edu>
2019-06-06 10:44:00 -0400

    added routing for the pages

---
+++ task-manager/src/task-pages/CreateTask.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function CreateTask() {
+
+    return (
+        <div>
+            <p>create task</p>
+        </div>
+    )
+}
---
+++ task-manager/src/task-pages/ViewTasks.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function ViewTasks() {
+
+    return (
+        <div>
+            <p>view tasks</p>
+        </div>
+    )
+}
